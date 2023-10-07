c3fcc1681c121b9085736a4e087f4c25ccd85688
prince soni <soniprince142@gmail.com>
2023-10-07 20:12:35 +0530

    vercel update7

--- src/index.js
+++ src/index.js
@@ -2,22 +2,38 @@ let express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) =>
-  res.send(`<h1 style="@keyframes colorChange {
-    0% {
-      color: #ff5733; /* Start color (orange) */
-    }
-    50% {
-      color: #c70039; /* Middle color (red) */
-    }
-    100% {
-      color: #900c3f;
-    }
-  }
-  .animated-text {
-    font-size: 3em;
-    animation: colorChange 0.5s infinite alternate;
-  }">Hello Project King!</h1>`))
-app.get('/a', (req, res) => res.send('Hello World! Ji')
-)
+app.get('/', (req, res) => {
+  res.send(`
+      <!DOCTYPE html>
+      <html lang="en">
+      <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Welcome</title>
+        <style>
+          @keyframes colorChange {
+            0% {
+              color: #ff5733; /* Start color (orange) */
+            }
+            50% {
+              color: #c70039; /* Middle color (red) */
+            }
+            100% {
+              color: #900c3f; /* End color (dark red) */
+            }
+          }
+  
+          .animated-text {
+            font-size: 3em;
+            animation: colorChange 0.5s infinite alternate;
+          }
+        </style>
+      </head>
+      <body>
+        <h1 class="animated-text">Hello Project King!</h1>
+      </body>
+      </html>
+    `)
+})
+app.get('/a', (req, res) => res.send('Hello World! Ji'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
