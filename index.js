3bd0857bf49035aae21ac6282302956b366b9211
prince soni <soniprince142@gmail.com>
2023-10-07 19:24:50 +0530

    vercel update1

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ let express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.get('/', (req, res) => res.send('Hello Project!'))
 app.get('/a', (req, res) => res.send('Hello World! Ji'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
